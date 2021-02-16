c10247dc2750d06fa3501eef95e776f766ed3bc6
sandeepkumarbolla <sandeepchowdarybolla159@gmail.com>
2021-02-16 15:59:59 +0530

    Create Assignment4Q8.java

---
+++ CoreJava/Lambda expression Assignments/Assignment4Q8.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Assignment4Q8 {
+	public static void main(String[] arg) {
+		 List<Integer> list = new ArrayList<Integer>();
+	   	 Scanner ss= new Scanner(System.in);
+	   	 System.out.print("Enter the number of elements: ");
+	   	 int n= ss.nextInt();
+	   	 for(int i=0;i<n;i++)
+	   	 {
+	   		 System.out.print("enter the element "+(i+1)+" in the list : ");
+	   		 int number= ss.nextInt();
+	   		 list.add(number);
+	   	 }
+		 Thread t = new Thread(new ThreadDemo(list)); 
+		 t.start();
+	}
+}
+
+class ThreadDemo extends Thread
+{
+	List<Integer> list = new ArrayList<Integer>();
+	ThreadDemo(List<Integer> l){list=l;}
+	Consumer<List<Integer> > dispList = list -> list.stream().forEach(a -> System.out.print(a + " "));
+	public void run()
+	{ 
+	  dispList.accept(list); 
+	}
+}
