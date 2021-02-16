f07338467eb9b36038f0bcf66748eb8f0c9a12a6
sandeepkumarbolla <sandeepchowdarybolla159@gmail.com>
2021-02-16 12:38:51 +0530

    Create Assignment1Q8.java

---
+++ CoreJava/BasicDataStructure/Assignment1Q8.java
@@ -0,0 +1,31 @@
+class BubbleSort 
+{
+    public int[] bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j < (n - i); j++) 
+            {
+                if (arr[j - 1] > arr[j]) 
+                {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    return arr;
+    }
+}
+public class Assignment1Q8 {
+    public static void main(String args[]) {
+        int arr[] = {5, 12, 14, 6, 78, 19, 1, 23, 26, 35, 37, 7, 52, 86, 47};
+        BubbleSort sort = new BubbleSort();
+        sort.bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
